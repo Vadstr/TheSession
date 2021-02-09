988cdd58aa37d98ace2b6f374d6262a923ac85a9
Vadstr <StriletskiyVadim@gmail.com>
2021-02-09 18:54:10 +0200

    creat main menu

---
+++ Assets/Scripts/Menu/MenuBatton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBatton : MonoBehaviour
+{
+    public void NewGame()
+    {
+    }
+
+    public void ContinueGame() 
+    {
+    }
+
+    public void Authors()
+    {
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeLanguage() 
+    {
+        LanguageID.languageID += 1;
+    }
+}
---
+++ Assets/Scripts/Menu/MenuText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuText : MonoBehaviour
+{
+
+    public Text text;
+    public int textId;
+    public int languageID;
+
+    void Update()
+    {
+        int languageID = LanguageID.languageID % 3;
+        string[,] allText = {{"En", "RU", "UA"},
+                             {"The session", "Сессия", "Сесія"},
+                             {"New Game", "Новая игра", "Нова гра"},
+                             {"Continue", "Продолжить", "Продовжити"},
+                             {"Autors", "Авторы", "Автори" },
+                             {"Exit", "Выход", "Вихід" } };
+        text.text = allText[textId, languageID];
+    }
+}
