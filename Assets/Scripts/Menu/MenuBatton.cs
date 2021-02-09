4352544c5b8358beea3121f84e5132a80cd633ef
Vadstr <StriletskiyVadim@gmail.com>
2021-02-09 23:33:49 +0200

    Transition from Main menu
    add animation and methot to moov from main menu

--- Assets/Scripts/Menu/MenuBatton.cs
+++ Assets/Scripts/Menu/MenuBatton.cs
@@ -1,28 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class MenuBatton : MonoBehaviour
 {
+    public GameObject panel;
+
     public void NewGame()
     {
+        panel.GetComponent<Animator>().SetTrigger("Transition");
     }
 
     public void ContinueGame() 
     {
+        panel.GetComponent<Animator>().SetTrigger("Transition");
     }
 
     public void Authors()
     {
+        panel.GetComponent<Animator>().SetTrigger("Transition");
+        PrintAutors();
+        panel.GetComponent<Animator>().SetTrigger("Back");
     }
 
     public void Exit()
     {
+        panel.GetComponent<Animator>().SetTrigger("Transition");
         Application.Quit();
     }
 
     public void ChangeLanguage() 
     {
         LanguageID.languageID += 1;
+    }
+
+    private void PrintAutors()
+    {
     }
 }
